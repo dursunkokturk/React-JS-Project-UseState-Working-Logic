28dbc0322e80253daca27c09069d8de025f616e6
Dursun Köktürk <dursunserefkokturk@gmail.com>
2024-09-06 13:17:09 +0300

    useState-working-logic

--- src/App.js
+++ src/App.js
@@ -2,32 +2,20 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-
-  // Value Degiskeni Normalde Var Olan Degerin Bulundugu Degiskendir
-  // setValue Degiskeni Guncelleme Isleminden Sonra Gelecek Olan Degerin Bulundugu Degiskendir
-  // useState ten Sonra Parantez Icinde Yazilan Deger UseState in Baslangic Degeridir 
-  const [value, setValue] = useState(0);
-
-  // Fonksiyon Icinde Yapilacak Islemi Tanimliyoruz
-  const handleClick = () => {
-
-    // Guncelleme Isleminin Yapidigi Satir
-    setValue(value + 1);
-  };
-  return (
-    <div className="App">
-
-      {/* Butona Tiklandiginda 
-          handleClick Fonksiyonunu Cagiriyoruz */}
-      <button onClick={handleClick}>Kurs Ekle</button>
-
-      {/* Butona Tiklandiginda 
-          Cagirilan Fonksiyonda 
-          Yapilan Islemin Sonucunu Yazdiriyoruz */}
-      <div>Kurs Sayısı:{value}</div>
-
-    </div>
-  );
+    const [value, setValue] = useState(0);
+    const handleClick = () => {
+        setValue(value + 1);
+    };
+    return (
+        <div className="App">
+            <button onClick={handleClick}>
+                Kurs Ekle
+            </button>
+            <div>
+                Kurs Sayısı:{value}
+            </div>
+        </div>
+    );
 }
 
 export default App;
